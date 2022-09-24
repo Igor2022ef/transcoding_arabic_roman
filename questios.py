b7437ac4448ec31e887677c3e549c6c9d1b15923
Ilia1794 <poroshin_io@mail.ru>
2022-09-24 14:23:11 +0300

    add some files

---
+++ questios.py
@@ -0,0 +1,44 @@
+#Генератор чисел Фибоначи
+def gen_f(a,b):
+    ans = []
+    c = 0
+    while c < 11:
+        b, a = a, a+b #Как же это работает?
+        c = c + 1
+        ans.append(a)
+    del ans[0]
+    return (ans)
+
+def gen_f_1(a_1,b_1):
+    ans_1 = []
+    s = 0
+    while s < 11:
+        c_1 = a_1 + b_1
+        ans_1.append(c_1)
+        a_1 = b_1
+        b_1 = c_1
+        s = s + 1
+    return (ans_1)
+
+def gen_f_2(a_2,b_2):
+    ans_2 = []
+    m = 0
+    while m < 11:
+        c_2 = a_2 + b_2
+        if c_2 % 2 != 0:
+            ans_2.append(c_2)
+        a_2 = b_2
+        b_2 = c_2
+        m = m + 1
+    return (ans_2)
+
+
+num = [_*2+1 for _ in range(5)]
+print(num)                             #Почему интерпретатор выводит этот генератор первым из 4 прог?
+
+
+
+if __name__ == "__main__":
+    print(gen_f(0,1))
+    print(gen_f_1(0, 1))
+    print(gen_f_2(0, 1))
---
+++ roman_numbersMY_old.py
@@ -0,0 +1,85 @@
+'''
+Перевод числа, написанного арабскими цифрами в риские.
+num - Исходное число
+Пока реализовано до 399, далее конфликт
+'''
+
+def rom(num: int):
+    if not isinstance(num, int) is True or num == 0:
+        raise SystemExit('Это значение не конвертируемо') #Верно ли сделал остановку при ошибке, что делать с числом 01?
+    if num >= 1000:
+        raise SystemExit('M - и все на этом')              #Последнее число, не знаю, как верно оформить этот шаг
+    res = list(map(int, str(num)))
+    rim = []
+    if len(res) < 2:
+        num = Num_first(res[0])
+    if 2 <= len(res) < 3:
+        num = Num_second(res[1],res[0])
+        dig2_sum = num.dig2()
+        rim.append(dig2_sum)
+    if 3 <= len(res) < 4:
+        num = Num_second(res[2], res[1])
+        if res[0] < 4:
+            first_n = res[0] * 'C'
+        if 4 <= res[0] < 5:
+            first_n = 'CD'
+        if 5 <= res[0] < 6:
+            first_n = 'D'
+        if 6 <= res[0] < 9:
+            first_n = ('D' + 'C'*(res[0] - 5))
+        if 9 <= res[0]:
+            first_n = 'CM'
+        if num.dig() != None:
+            dig2_sum = first_n + num.dig2() + num.dig()
+        dig2_sum = first_n + num.dig2()
+        rim.append(dig2_sum)
+    if num.dig() != None:
+        rim.append(num.dig())
+    print(f'Это результат со всеми разрядами: {"".join(rim)}')
+
+class Num_first():
+#    Число, соответствующее арабскому 0 - 9, первый разряд
+
+    def __init__(self,num_min):
+        self.num_min = num_min
+    def dig(self):    
+        nessery_n = ''
+        if 1 <= self.num_min <= 3:
+            nessery_n += self.num_min*'I'
+            return nessery_n 
+        if self.num_min == 4:
+            nessery_n = 'IV'
+            return nessery_n 
+        if self.num_min == 5:
+            nessery_n += 'V'
+            return nessery_n     
+        if 5 < self.num_min <= 8:
+            nessery_n += 'V'+(self.num_min - 5)*'I'
+            return nessery_n     
+        if self.num_min == 9:
+            nessery_n += "IX"
+            return nessery_n
+
+class Num_second(Num_first):
+#       Число, соответствующее арабскому 10 - 999, старшие разряды
+
+    def __init__(self,num_min, num_mid):
+        super().__init__(num_mid) 
+        self.num_min = num_min
+        self.num_mid = num_mid
+    def dig2(self):
+        if self.num_mid < 4:
+            nessery_m = "X" * self.num_mid
+            return nessery_m
+        if self.num_mid == 4:
+            nessery_m = "XL"
+            return nessery_m
+        if 5 <= self.num_mid <= 8:
+            nessery_m = ('L' + 'X'*(self.num_mid - 5))
+            return nessery_m
+        if self.num_mid == 9:
+            nessery_m = "XC"
+            return nessery_m
+
+if __name__ == "__main__":
+    rom(100)
